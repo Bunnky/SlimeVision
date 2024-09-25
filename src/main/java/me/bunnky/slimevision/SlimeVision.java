790741b4c37668871c5368f1c423032d53d64149
Bill <bunnkywhat@gmail.com>
2024-09-25 17:28:40 -0400

    Add more metrics; advanced pie charts

--- src/main/java/me/bunnky/slimevision/SlimeVision.java
+++ src/main/java/me/bunnky/slimevision/SlimeVision.java
@@ -1,13 +1,18 @@
 package me.bunnky.slimevision;
 
 import fr.skytasul.glowingentities.GlowingBlocks;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.updater.BlobBuildUpdater;
+import me.bunnky.slimevision.items.slimeeyes.SlimeEye;
 import me.bunnky.slimevision.slimefun.Setup;
 import org.bstats.bukkit.Metrics;
+import org.bstats.charts.AdvancedPie;
 import org.bukkit.plugin.java.JavaPlugin;
 import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 
 import javax.annotation.Nonnull;
 import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SlimeVision extends JavaPlugin implements SlimefunAddon {
     private static SlimeVision instance;
@@ -31,18 +36,40 @@ public class SlimeVision extends JavaPlugin implements SlimefunAddon {
         getLogger().info("       by Bunnky       ");
 
         saveDefaultConfig();
+        tryUpdate();
 
         glowingBlocks = new GlowingBlocks(this);
 
         Setup.setup();
-        new Metrics(this, 23251);
+        setupMetrics();
+    }
 
+    public void setupMetrics (){
+        Metrics metrics = new Metrics(this, 23251);
+
+        AdvancedPie playersChart = new AdvancedPie("slimeeye_users", () -> {
+            Map<String, Integer> vmap = new HashMap<>();
+            SlimeEye.getUsers().forEach(users -> vmap.put(users, 1));
+            return vmap;
+        });
+
+        AdvancedPie eyeChart = new AdvancedPie("slimeeye_types", SlimeEye::getSlimeEyeUsage);
+
+        metrics.addCustomChart(playersChart);
+        metrics.addCustomChart(eyeChart);
+    }
+
+    public void tryUpdate() {
+        if (getConfig().getBoolean("options.auto-update", true)
+            && getDescription().getVersion().startsWith("Dev - ")
+        ) {
+            new BlobBuildUpdater(this, getFile(), "SlimeVision", "Dev").start();
+        }
     }
 
     public static void consoleMsg(@Nonnull String string) {
         instance.getLogger().info(string);
     }
-
 
     public static SlimeVision getInstance() {
         return instance;
